1a60763bded3629beb0bd10d7c42149d90aeb9a8
andrebellu <bellu.andrea@outlook.it>
2023-03-02 08:03:02 +0100

    typos

--- src/lib/components/homepage/cards/Cards.svelte
+++ src/lib/components/homepage/cards/Cards.svelte
@@ -7,21 +7,17 @@
 <div class="flex flex-row gap-4 justify-center md:w-screen md:flex-wrap">
 	<div class="pl-2 flex flex-col gap-4 md:flex-row">
 		{#each $recipes as recipe}
-			{#if recipe.data.category.includes($pressed)}
-				{#if $recipes.indexOf(recipe) % 2 == 0}
-					<Card {recipe} />
-				{/if}
+			{#if $recipes.indexOf(recipe) % 2 == 0}
+				<Card {recipe} />
 			{/if}
 		{/each}
 	</div>
 
 	<div class="pr-4 flex flex-col gap-4 md:flex-row md:p-2 md:flex-wrap">
-		<RecipesFound />
+		<!--<RecipesFound />-->
 		{#each $recipes as recipe}
-			{#if recipe.data.category.includes($pressed)}
-				{#if $recipes.indexOf(recipe) % 2 != 0}
-					<Card {recipe} />
-				{/if}
+			{#if $recipes.indexOf(recipe) % 2 != 0}
+				<Card {recipe} />
 			{/if}
 		{/each}
 	</div>
