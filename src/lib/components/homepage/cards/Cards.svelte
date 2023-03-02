fdb185d21c5ce4e90c14de96303d326f7a2579f4
andrebellu <bellu.andrea@outlook.it>
2023-03-02 09:40:20 +0100

    added filters

--- src/lib/components/homepage/cards/Cards.svelte
+++ src/lib/components/homepage/cards/Cards.svelte
@@ -13,8 +13,14 @@
 		{/each}
 	</div>
 
-	<div class="pr-4 flex flex-col gap-4 md:flex-row md:p-2 md:flex-wrap">
-		<!--<RecipesFound />-->
+	<div class="pr-2 flex flex-col gap-4 md:flex-row md:p-2 md:flex-wrap">
+		<div class="filters flex gap-x-1 py-2">
+			<span class="material-symbols-outlined badge p-4 text-xl text-secondary">
+				local_fire_department
+			</span>
+			<span class="material-symbols-outlined badge p-4 text-xl text-secondary"> visibility </span>
+			<span class="material-symbols-outlined badge p-4 text-xl text-secondary"> trending_up </span>
+		</div>
 		{#each $recipes as recipe}
 			{#if $recipes.indexOf(recipe) % 2 != 0}
 				<Card {recipe} />
