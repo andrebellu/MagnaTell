6784642301beb5609239267a956aef0b5545450b
Francesco Annunziata <annu@MacBook-Air-di-Francesco.local>
2023-03-29 09:09:33 +0200

    Filter style changed

--- src/lib/components/homepage/cards/Cards.svelte
+++ src/lib/components/homepage/cards/Cards.svelte
@@ -17,11 +17,11 @@
 
 	<div class="pr-2 flex flex-col gap-4 md:flex-row md:p-2 md:flex-wrap">
 		<div class="filters flex gap-x-1 py-2">
-			<span class="badge p-4 text-xl text-secondary">
+			<span class="badge rounded-r-lg p-4 text-xl text-secondary">
 				<Fire/>
 			</span>
-			<span class="badge p-4 text-xl text-secondary"> <Eye /> </span>
-			<span class="badge p-4 text-xl text-secondary"> <TrendUp /> </span>
+			<span class="badge rounded-lg p-4 text-xl text-secondary"> <Eye /> </span>
+			<span class="badge rounded-l-lg p-4 text-xl text-secondary"> <TrendUp /> </span>
 		</div>
 		{#each $recipes as recipe}
 			{#if $recipes.indexOf(recipe) % 2 != 0}
