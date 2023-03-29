7923c62530b7725baac478ffcbcb46cd5dad0f00
andrebellu <bellu.andrea@outlook.it>
2023-03-29 09:36:20 +0200

    loading carousel

--- src/lib/components/homepage/cards/Cards.svelte
+++ src/lib/components/homepage/cards/Cards.svelte
@@ -1,7 +1,8 @@
 <script>
 	import { recipes } from '../../../../routes/stores/store';
 	import Card from '$lib/components/homepage/card/Card.svelte';
-	import Filters from '../filters/Filters.svelte';
+	import Filters from '$lib/components/homepage/filters/Filters.svelte';
+	import LoadingFilters from '$lib/components/loading/filters/LoadingFilters.svelte';
 </script>
 
 <div class="flex flex-row gap-4 justify-center md:w-screen md:flex-wrap">
@@ -14,7 +15,11 @@
 	</div>
 
 	<div class="pr-2 flex flex-col gap-4 md:flex-row md:p-2 md:flex-wrap">
-		<Filters />
+		{#if $recipes.length == 0}
+			<LoadingFilters />
+		{:else}
+			<Filters />
+		{/if}
 		{#each $recipes as recipe}
 			{#if $recipes.indexOf(recipe) % 2 != 0}
 				<Card {recipe} />
---
+++ src/lib/components/loading/filters/LoadingFilters.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Fire from 'phosphor-svelte/lib/Fire';
+	import Eye from 'phosphor-svelte/lib/Eye';
+	import TrendUp from 'phosphor-svelte/lib/TrendUp';
+</script>
+
+<div class="filters flex gap-x-1 py-2">
+	<span
+		class="badge rounded-r-lg p-4 text-xl animate-pulse bg-gray-200 dark:bg-accent text-gray-200"
+		><Fire />
+	</span>
+	<span class="badge rounded-lg p-4 text-xl animate-pulse bg-gray-200 dark:bg-accent text-gray-200"
+		><Eye />
+	</span>
+	<span
+		class="badge rounded-l-lg p-4 text-xl animate-pulse bg-gray-200 dark:bg-accent text-gray-200"
+	>
+		<TrendUp />
+	</span>
+</div>
