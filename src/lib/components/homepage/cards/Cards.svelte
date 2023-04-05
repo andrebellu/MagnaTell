3fdf64c2f2f355e361127f1a33bdb4ff04412e4a
fabiooo4 <fabioirimie@gmail.com>
2023-04-05 08:54:18 +0200

    added card loading placeholder

--- src/lib/components/homepage/cards/Cards.svelte
+++ src/lib/components/homepage/cards/Cards.svelte
@@ -3,27 +3,28 @@
 	import Card from '$lib/components/homepage/card/Card.svelte';
 	import Filters from '$lib/components/homepage/filters/Filters.svelte';
 	import LoadingFilters from '$lib/components/loading/filters/LoadingFilters.svelte';
+	import LoadingCards from '$lib/components/loading/cards/LoadingCards.svelte';
 </script>
 
 <div class="flex flex-row gap-4 justify-center md:w-screen md:flex-wrap">
-	<div class="pl-2 flex flex-col gap-4 md:flex-row">
-		{#each $recipes as recipe}
-			{#if $recipes.indexOf(recipe) % 2 == 0}
-				<Card {recipe} />
-			{/if}
-		{/each}
-	</div>
+	{#if $recipes.length == 0}
+		<LoadingCards />
+	{:else}
+		<div class="pl-2 flex flex-col gap-4 md:flex-row">
+			{#each $recipes as recipe}
+				{#if $recipes.indexOf(recipe) % 2 == 0}
+					<Card {recipe} />
+				{/if}
+			{/each}
+		</div>
 
-	<div class="pr-2 flex flex-col gap-4 md:flex-row md:p-2 md:flex-wrap">
-		{#if $recipes.length == 0}
-			<LoadingFilters />
-		{:else}
+		<div class="pr-2 flex flex-col gap-4 md:flex-row md:p-2 md:flex-wrap">
 			<Filters />
-		{/if}
-		{#each $recipes as recipe}
-			{#if $recipes.indexOf(recipe) % 2 != 0}
-				<Card {recipe} />
-			{/if}
-		{/each}
-	</div>
+			{#each $recipes as recipe}
+				{#if $recipes.indexOf(recipe) % 2 != 0}
+					<Card {recipe} />
+				{/if}
+			{/each}
+		</div>
+	{/if}
 </div>
