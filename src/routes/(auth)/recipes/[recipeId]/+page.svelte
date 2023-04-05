719ac79c1211b38ed43cc19fbeb3815e96b8c9f5
andrebellu <bellu.andrea@outlook.it>
2023-04-05 11:10:28 +0200

    fixed video player, added carousel for recipe details

--- src/routes/(auth)/recipes/[recipeId]/+page.svelte
+++ src/routes/(auth)/recipes/[recipeId]/+page.svelte
@@ -45,11 +45,20 @@
 		});
 
 		link = recipe.link;
+
+		console.log(link);
 		if (link.includes('youtube')) {
 			link = link.replace('watch?v=', 'embed/');
+
+			if (link.includes('&')) {
+				link = link.split('&')[0];
+				console.log(link);
+			}
 		} else if (!link.includes('https://') && !link.includes('http://')) {
 			link = 'https://' + link;
 		}
+
+		console.log(link);
 	});
 
 	const recipesDelete = () => {
@@ -62,82 +71,111 @@
 {#if recipe.title === undefined}
 	<LoadingRecipe />
 {:else}
-	<div class="recipe p-4">
-		<h1 class="text-5xl text-center font-bold">{recipe.title}</h1>
+	<div class="recipe p-4 flex flex-col justify-center h-full">
+		<div class="title flex">
+			<img src={cover} alt={recipe.title} class="w-32 h-32 object-cover rounded-xl mb-2" />
+			<div class="flex flex-col ml-4">
+				<h1 class="text-3xl font-bold">{recipe.title}</h1>
 
-		<div class="flex flex-row justify-center">
-			{#each categories as category}
-				<span class="badge badge-primary font-bold mx-1 my-2">{category}</span>
-			{/each}
-		</div>
+				<h1>{recipe.author}</h1>
 
-		<img src={cover} alt={recipe.title} class="w-full h-40 object-cover rounded-xl mb-2" />
-
-		<div class="pt-2 flex justify-center"><Stars /></div>
-
-		<h1 class="text-3xl font-bold pt-3 pb-1">General Informations</h1>
-		<h1><strong>Author:</strong> {recipe.author}</h1>
-		<h1>
-			<strong>Difficulty:</strong>
-			{String(recipe.difficulty).charAt(0).toUpperCase() + String(recipe.difficulty).slice(1)}
-		</h1>
-		<h1><strong>Portions:</strong> {recipe.portions}</h1>
-		<h1><strong>Preparation time:</strong> {recipe.time} min</h1>
-
-		<h2 class="text-2xl font-bold pt-3 pb-1">Description:</h2>
-		<h1>{recipe.description}</h1>
-
-		<h1 class="text-3xl font-bold pt-6 pb-1">Ingredients:</h1>
-		<div class="ingredients pt-1">
-			{#each ingredients as ingredient}
-				<div class="grid grid-cols-2 grid-rows-1 gap-2">
-					<h1 class="font-semibold">
-						• {String(ingredient.ingredient).charAt(0).toUpperCase() +
-							String(ingredient.ingredient).slice(1)}
-					</h1>
-					<h1>{ingredient.quantity}</h1>
+				<div class="flex flex-row justify-center">
+					{#each categories as category}
+						<span class="badge badge-primary font-bold my-2 mr-2">{category}</span>
+					{/each}
 				</div>
-			{/each}
+
+				<div><Stars /></div>
+			</div>
 		</div>
 
-		<h1 class="text-3xl font-bold pt-6 pb-1">Steps:</h1>
+		<div class="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box h-96">
+			<div class="carousel-item flex flex-col w-full" id="general">
+				<h1 class="text-3xl font-bold pb-1">Infos</h1>
+				<h1>
+					<strong>Difficulty:</strong>
+					{String(recipe.difficulty).charAt(0).toUpperCase() + String(recipe.difficulty).slice(1)}
+				</h1>
+				<h1><strong>Portions:</strong> {recipe.portions}</h1>
+				<h1><strong>Preparation time:</strong> {recipe.time} min</h1>
 
-		{#if steps.length > 0}
-			{#each steps as step}
-				{#if step}
-					<h1><strong>{step.number}</strong> {step.text}</h1>
+				<h2 class="text-2xl font-bold pt-3 pb-1">Description:</h2>
+				<h1>{recipe.description}</h1>
+			</div>
+
+			<div class="carousel-item flex flex-col w-full" id="ingredients">
+				<h1 class="text-3xl font-bold pb-1">Ingredients:</h1>
+				<div class="ingredients pt-1">
+					{#each ingredients as ingredient}
+						<div class="grid grid-cols-2 grid-rows-1 gap-2">
+							<h1 class="font-semibold">
+								• {String(ingredient.ingredient).charAt(0).toUpperCase() +
+									String(ingredient.ingredient).slice(1)}
+							</h1>
+							<h1>{ingredient.quantity}</h1>
+						</div>
+					{/each}
+				</div>
+			</div>
+
+			<div class="carousel-item flex flex-col w-full" id="steps">
+				<h1 class="text-3xl font-bold pb-1">Steps:</h1>
+
+				{#if steps.length > 0}
+					{#each steps as step}
+						{#if step}
+							<h1><strong>{step.number}</strong> {step.text}</h1>
+						{/if}
+					{/each}
+				{:else}
+					<h1>{recipe.steps}</h1>
 				{/if}
-			{/each}
-		{:else}
-			<h1>{recipe.steps}</h1>
-		{/if}
+			</div>
 
-		{#if link}
-			<h1 class="text-3xl font-bold pt-6 pb-1">Useful links:</h1>
-		{/if}
+			<div class="carousel-item flex flex-col w-full" id="links">
+				{#if link}
+					<h1 class="text-3xl font-bold pb-1">Useful links:</h1>
+				{/if}
 
-		{#if link.includes('youtube')}
-			<iframe
-				src={link}
-				title="YouTube video player"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				allowfullscreen
-				class="w-full h-64 rounded-xl"
-			/>
-		{:else}
-			<a href={link} class="btn">Link</a>
-		{/if}
+				{#if link.includes('youtube')}
+					<iframe
+						src={link}
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen
+						class="w-full h-64 rounded-xl"
+					/>
+				{:else}
+					<a href={link} class="btn">Link</a>
+				{/if}
+			</div>
+		</div>
 
-		{#if recipe.uid == auth.currentUser.uid}
-			<button on:click={recipesDelete} class="btn btn-danger">Delete</button>
-		{/if}
+		<ul class="steps mt-4">
+			{#if $page.url.hash === '' || $page.url.hash === '#general'}
+				<a class="step step-primary" href="#general">General</a>
+			{:else}
+				<a class="step" href="#general">General</a>
+			{/if}
+
+			{#if $page.url.hash === '#ingredients'}
+				<a class="step step-primary" href="#ingredients">Ingredients</a>
+			{:else}
+				<a class="step" href="#ingredients">Ingredients</a>
+			{/if}
+
+			{#if $page.url.hash === '#steps'}
+				<a class="step step-primary" href="#steps">Steps</a>
+			{:else}
+				<a class="step" href="#steps">Steps</a>
+			{/if}
+
+			{#if $page.url.hash === '#links'}
+				<a class="step step-primary" href="#links">Links</a>
+			{:else}
+				<a class="step" href="#links">Links</a>
+			{/if}
+		</ul>
 	</div>
-
-	<ul class="steps">
-		<li class="step step-primary">General</li>
-		<li class="step step-primary">Ingredients</li>
-		<li class="step">Steps</li>
-		<li class="step">Links</li>
-	</ul>
 {/if}
