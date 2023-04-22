6824dc36183c1cd1cbe8c030835a476699ff974e
Leonardo Baccalario <leonardobaccalario@gmail.com>
2023-04-22 16:53:50 +0200

    Update +server.js

--- src/routes/api/cookies/+server.js
+++ src/routes/api/cookies/+server.js
@@ -1,5 +1,4 @@
 import admin from '$lib/firebase/firebase-admin.js';
-import { auth } from '$lib/firebase/firebase.client.js';
 import { dev } from '$app/environment';
 
 const expiresIn = 1000 * 60 * 60 * 24;
@@ -9,12 +8,10 @@ export const POST = async (event) => {
     const { idToken } = await event.request.json();
     try {
         const sessionCookie = await admin.auth().createSessionCookie(idToken, { expiresIn });
-        const a = auth.currentUser.uid;
         return new Response(JSON.stringify({ sessionCookie }), {
             status: 200,
             headers: {
-                "Set-Cookie": `idToken=${sessionCookie}; Max-Age=${expiresIn}; HttpOnly; Path=/; ${secure}; SameSite=Strict;`,
-                "Set-Cookie": `uid=${a}; Max-Age=${expiresIn}; HttpOnly; Path=/; ${secure}; SameSite=Strict;`
+                "Set-Cookie": `idToken=${sessionCookie}; Max-Age=${expiresIn}; HttpOnly; Path=/; ${secure}; SameSite=Strict;`
             }
         })
     } catch (error) {
